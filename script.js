4d236d5c79ad6195f3240302b680c6774392ddf6
Mehul Mohan <mehulmpt@gmail.com>
2020-12-09 23:49:36 +0530

    lab added

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
-function sum(arr, n) {
-  // Only change code below this line
+// Setup
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["JavaScript", "Gaming", "Foxes"]
+    }
+];
 
-  // Only change code above this line
+
+function lookUpProfile(name, prop){
+// Only change code below this line
+
+// Only change code above this line
 }
+
+lookUpProfile("Akira", "likes");
