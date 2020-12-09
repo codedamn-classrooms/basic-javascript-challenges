f9a3dfbb403d214aa6fd96b57f6c1af8ff24b65c
Mehul Mohan <mehulmpt@gmail.com>
2020-12-09 23:49:32 +0530

    lab added

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
-var myNoun = "dog";
-var myAdjective = "big";
-var myVerb = "ran";
-var myAdverb = "quickly";
-
 // Only change code below this line
-var wordBlanks = ""; // Change this line
-// Only change code above this line
+var myArray = [];
