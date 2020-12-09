303098894a391756134826c19c920b105a1cb5a5
Mehul Mohan <mehulmpt@gmail.com>
2020-12-09 23:49:35 +0530

    lab added

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-var count = 0;
-
-function cc(card) {
-  // Only change code below this line
+var myDog = {
+// Only change code below this line
 
 
-  return "Change Me";
-  // Only change code above this line
-}
-
-cc(2); cc(3); cc(7); cc('K'); cc('A');
+// Only change code above this line
+};
