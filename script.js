84969579517c118a11cee453a429758bff03640e
Mehul Mohan <mehulmpt@gmail.com>
2020-12-09 23:49:36 +0530

    lab added

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-// Setup
-var myArr = [ 2, 3, 4, 5, 6];
+function multiplyAll(arr) {
+  var product = 1;
+  // Only change code below this line
 
-// Only change code below this line
+  // Only change code above this line
+  return product;
+}
+
+multiplyAll([[1,2],[3,4],[5,6,7]]);
