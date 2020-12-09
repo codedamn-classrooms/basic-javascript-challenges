76e3ce7b724728192401d7328b0d65c397c475fe
Mehul Mohan <mehulmpt@gmail.com>
2020-12-09 23:49:35 +0530

    lab added

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 // Setup
-var testObj = {
-  12: "Namath",
-  16: "Montana",
-  19: "Unitas"
+var myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
 };
 
 // Only change code below this line
-
-var playerNumber;       // Change this line
-var player = testObj;   // Change this line
