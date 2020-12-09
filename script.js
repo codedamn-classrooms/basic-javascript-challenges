faabfd5c1947dc1c76bc9912d4c7a59cfbdaf808
Mehul Mohan <mehulmpt@gmail.com>
2020-12-09 23:49:36 +0530

    lab added

--- script.js
+++ script.js
@@ -1,27 +1,4 @@
 // Setup
-var collection = {
-  2548: {
-    albumTitle: 'Slippery When Wet',
-    artist: 'Bon Jovi',
-    tracks: ['Let It Rock', 'You Give Love a Bad Name']
-  },
-  2468: {
-    albumTitle: '1999',
-    artist: 'Prince',
-    tracks: ['1999', 'Little Red Corvette']
-  },
-  1245: {
-    artist: 'Robert Palmer',
-    tracks: []
-  },
-  5439: {
-    albumTitle: 'ABBA Gold'
-  }
-};
+var myArray = [];
 
 // Only change code below this line
-function updateRecords(object, id, prop, value) {
-  return object;
-}
-
-updateRecords(collection, 5439, 'artist', 'ABBA');
