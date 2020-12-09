364a3df729903734d1eb3c2589a2480c10c39651
Mehul Mohan <mehulmpt@gmail.com>
2020-12-09 23:49:36 +0530

    lab added

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 // Setup
-var myPlants = [
-  {
-    type: "flowers",
-    list: [
-      "rose",
-      "tulip",
-      "dandelion"
-    ]
+var collection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
   },
-  {
-    type: "trees",
-    list: [
-      "fir",
-      "pine",
-      "birch"
-    ]
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
   }
-];
+};
 
 // Only change code below this line
+function updateRecords(object, id, prop, value) {
+  return object;
+}
 
-var secondTree = ""; // Change this line
+updateRecords(collection, 5439, 'artist', 'ABBA');
