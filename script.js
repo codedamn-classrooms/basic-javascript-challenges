f83051bead889cd3f082aca531e0a7b161454bef
Mehul Mohan <mehulmpt@gmail.com>
2020-12-09 23:49:37 +0530

    lab added

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-function randomWholeNum() {
-
+function randomRange(myMin, myMax) {
   // Only change code below this line
-
-  return Math.random();
+  return 0;
+  // Only change code above this line
 }
