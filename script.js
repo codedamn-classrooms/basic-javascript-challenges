94af714bbc6ce208a12f12fac820e30d132b54e8
Mehul Mohan <mehulmpt@gmail.com>
2020-12-09 23:49:30 +0530

    lab added

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // Setup
-var lastNameLength = 0;
+var firstLetterOfLastName = "";
 var lastName = "Lovelace";
 
 // Only change code below this line
-
-lastNameLength = lastName;
+firstLetterOfLastName = lastName; // Change this line
