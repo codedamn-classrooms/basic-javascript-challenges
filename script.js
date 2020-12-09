1368aae495e98f514146fc3f325ac6cc839ac4eb
Mehul Mohan <mehulmpt@gmail.com>
2020-12-09 23:49:35 +0530

    lab added

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-function checkObj(obj, checkProp) {
-  // Only change code below this line
-  return "Change Me!";
-  // Only change code above this line
-}
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+  // Add a record here
+];
