4cd6a14745a6ddf09f74ece1a554def7bc0934e9
Mehul Mohan <mehulmpt@gmail.com>
2020-12-09 23:49:35 +0530

    lab added

--- script.js
+++ script.js
@@ -1,30 +1,5 @@
-// Setup
-function phoneticLookup(val) {
-  var result = "";
-
+function checkObj(obj, checkProp) {
   // Only change code below this line
-  switch(val) {
-    case "alpha":
-      result = "Adams";
-      break;
-    case "bravo":
-      result = "Boston";
-      break;
-    case "charlie":
-      result = "Chicago";
-      break;
-    case "delta":
-      result = "Denver";
-      break;
-    case "echo":
-      result = "Easy";
-      break;
-    case "foxtrot":
-      result = "Frank";
-  }
-
+  return "Change Me!";
   // Only change code above this line
-  return result;
 }
-
-phoneticLookup("charlie");
