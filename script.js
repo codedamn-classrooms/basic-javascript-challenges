7bea60f768c77bfd52d2b837c118fad46d904ede
Mehul Mohan <mehulmpt@gmail.com>
2020-12-09 23:49:33 +0530

    lab added

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-function nextInLine(arr, item) {
+function welcomeToBooleans() {
+
   // Only change code below this line
-  
-  return item;
+
+  return false; // Change this line
+
   // Only change code above this line
-  
-
 }
-
-// Setup
-var testArr = [1,2,3,4,5];
-
-// Display code
-console.log("Before: " + JSON.stringify(testArr));
-console.log(nextInLine(testArr, 6));
-console.log("After: " + JSON.stringify(testArr));
