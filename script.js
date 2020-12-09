c2b962619d775bd613d678364e98e0e1c2fc972d
Mehul Mohan <mehulmpt@gmail.com>
2020-12-09 23:49:37 +0530

    lab added

--- script.js
+++ script.js
@@ -1,36 +1,8 @@
-// Setup
-var contacts = [
-    {
-        "firstName": "Akira",
-        "lastName": "Laine",
-        "number": "0543236543",
-        "likes": ["Pizza", "Coding", "Brownie Points"]
-    },
-    {
-        "firstName": "Harry",
-        "lastName": "Potter",
-        "number": "0994372684",
-        "likes": ["Hogwarts", "Magic", "Hagrid"]
-    },
-    {
-        "firstName": "Sherlock",
-        "lastName": "Holmes",
-        "number": "0487345643",
-        "likes": ["Intriguing Cases", "Violin"]
-    },
-    {
-        "firstName": "Kristian",
-        "lastName": "Vos",
-        "number": "unknown",
-        "likes": ["JavaScript", "Gaming", "Foxes"]
-    }
-];
+function randomFraction() {
 
+  // Only change code below this line
 
-function lookUpProfile(name, prop){
-// Only change code below this line
+  return 0;
 
-// Only change code above this line
+  // Only change code above this line
 }
-
-lookUpProfile("Akira", "likes");
