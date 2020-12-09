1aad10890572fcab205b6fa49bd28a4d15a7393a
Mehul Mohan <mehulmpt@gmail.com>
2020-12-09 23:49:33 +0530

    lab added

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
-// Declare the myGlobal variable below this line
+function myLocalScope() {
 
+  // Only change code below this line
 
-function fun1() {
-  // Assign 5 to oopsGlobal Here
-
+  console.log('inside myLocalScope', myVar);
 }
+myLocalScope();
 
-// Only change code above this line
-
-function fun2() {
-  var output = "";
-  if (typeof myGlobal != "undefined") {
-    output += "myGlobal: " + myGlobal;
-  }
-  if (typeof oopsGlobal != "undefined") {
-    output += " oopsGlobal: " + oopsGlobal;
-  }
-  console.log(output);
-}
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+console.log('outside myLocalScope', myVar);
