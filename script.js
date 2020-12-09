5d7477cee62e378841caf9c38a47f22e40490545
Mehul Mohan <mehulmpt@gmail.com>
2020-12-09 23:49:34 +0530

    lab added

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-function testElseIf(val) {
-  if (val > 10) {
-    return "Greater than 10";
+function orderMyLogic(val) {
+  if (val < 10) {
+    return "Less than 10";
+  } else if (val < 5) {
+    return "Less than 5";
+  } else {
+    return "Greater than or equal to 10";
   }
-
-  if (val < 5) {
-    return "Smaller than 5";
-  }
-
-  return "Between 5 and 10";
 }
 
-testElseIf(7);
+orderMyLogic(7);
