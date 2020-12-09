0007955e3be988899441d1a0cf41eda2d809f00c
Mehul Mohan <mehulmpt@gmail.com>
2020-12-09 23:49:33 +0530

    lab added

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-function myLocalScope() {
+// Setup
+var outerWear = "T-Shirt";
 
+function myOutfit() {
   // Only change code below this line
 
-  console.log('inside myLocalScope', myVar);
-}
-myLocalScope();
 
-// Run and check the console
-// myVar is not defined outside of myLocalScope
-console.log('outside myLocalScope', myVar);
+
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
