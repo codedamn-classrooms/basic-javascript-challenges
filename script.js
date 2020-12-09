7df0eca6146bba108e6b23af2981ec0166c04210
Mehul Mohan <mehulmpt@gmail.com>
2020-12-09 23:49:33 +0530

    lab added

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-// Setup
-var outerWear = "T-Shirt";
-
-function myOutfit() {
-  // Only change code below this line
-
-
-
-  // Only change code above this line
-  return outerWear;
-}
-
-myOutfit();
+// your javascript file
