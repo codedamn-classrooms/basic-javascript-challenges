d4ce23b7daa5d807b951c96b84e867ca1266f381
Mehul Mohan <mehulmpt@gmail.com>
2020-12-09 23:49:35 +0530

    lab added

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
-function chainToSwitch(val) {
-  var answer = "";
+function isLess(a, b) {
   // Only change code below this line
-
-  if (val === "bob") {
-    answer = "Marley";
-  } else if (val === 42) {
-    answer = "The Answer";
-  } else if (val === 1) {
-    answer = "There is no #1";
-  } else if (val === 99) {
-    answer = "Missed me by this much!";
-  } else if (val === 7) {
-    answer = "Ate Nine";
+  if (a < b) {
+    return true;
+  } else {
+    return false;
   }
-
   // Only change code above this line
-  return answer;
 }
 
-chainToSwitch(7);
+isLess(10, 15);
