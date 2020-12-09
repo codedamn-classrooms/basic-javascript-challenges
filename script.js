a1a687f0ac5625d8a50e3ccbba895a39edd44aa4
Mehul Mohan <mehulmpt@gmail.com>
2020-12-09 23:49:33 +0530

    lab added

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 // Setup
-var sum = 0;
+var processed = 0;
 
-function addThree() {
-  sum = sum + 3;
+function processArg(num) {
+  return (num + 3) / 5;
 }
 
 // Only change code below this line
-
-
-// Only change code above this line
-
-addThree();
-addFive();
