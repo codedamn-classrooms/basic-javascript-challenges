a70df13a9764a11f7e9ba70b94b6f484af851176
Mehul Mohan <mehulmpt@gmail.com>
2020-12-09 23:49:35 +0530

    lab added

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-// Setup
-function abTest(a, b) {
+var count = 0;
+
+function cc(card) {
   // Only change code below this line
 
 
-
+  return "Change Me";
   // Only change code above this line
-
-  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
 }
 
-abTest(2,2);
+cc(2); cc(3); cc(7); cc('K'); cc('A');
